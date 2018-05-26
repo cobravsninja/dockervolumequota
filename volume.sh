c7b5c87238324e8357e3e19b82867c25b2403b50
cobravsninja <cobravsninja@gmail.com>
2018-05-26 16:14:52 +0400

    Update volume.sh

--- volume.sh
+++ volume.sh
@@ -8,21 +8,24 @@ mount_params='loop,rw,noatime,nodiratime'
 volume_dir=/var/lib/docker/volumes/$name
 
 error() {
-    echo "Usage: $(basename $0) size(MB) name"
+    [ -z "$1" ] && echo "Usage: $(basename $0) size(MB) name" || echo $1
     exit 1
 }
-    
+
 [[ ! $size =~ ^[0-9]+$ ]] || [[ ! $name =~ ^[a-zA-Z0-9_.-]+$ ]] && {
     error
 }
 
 [ -d $volume_dir ] && {
-    echo "Volume already exists in docker"
-    exit 1
+    error "Volume already exists in docker"
+}
+
+[ -f $location ] && {
+    error "File already exists"
 }
 
 docker volume create $name && {
-    dd of=$location bs=1M seek=$size count=0 && 
+    dd of=$location bs=1M seek=$size count=0 &&
     mkfs.ext4 -F $location &&
     mount -t ext4 -n -o $mount_params $location $volume_dir/_data &&
     echo "$location /var/lib/docker/volumes/$name/_data ext4 $mount_params 0 0" >> /etc/fstab
